a8bc541516b887834a6fbf67bfdcdb4fe0ee94f6
Kairat Yussupov <kairatbmstu@gmail.com>
2024-01-31 16:12:33 +0600

    Implemented frontend side;

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	http.Handle("/static", http.FileServer(http.Dir("./static")))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	http.HandleFunc("/", controller.GetIndexPage)
 
--- static/index.js
+++ static/index.js
@@ -1,38 +1,57 @@
-class PackSize {
-    constructor(size) {
-      this.size = size;
-    }
-  
-    // Getter for size
-    getSize() {
-      return this.size;
-    }
-  
-    // Method to perform some operation related to pack size
-    displayInfo() {
-      console.log(`Pack Size: ${this.size}`);
-    }
-}
+$(function () {
+  $("#submit-btn").click(function () {
+    // Collect data from form inputs
+    var packSizes = [];
+    $('input[name="packsize"]').each(function () {
+      packSizes.push($(this).val());
+    });
+
+    var packSizes = packSizes.map(Number);
+    console.log(packSizes);
+
+    $.ajax({
+      type: "POST",
+      url: "/submitPackSettings",
+      contentType: "application/json",
+      data: JSON.stringify(packSizes),
+      success: function (response) {
+        console.log(response);
+        // Handle success response
+      },
+      error: function (error) {
+        console.error(error);
+        // Handle error
+      }
+    });
+  });
 
 
-class Pack {
-    constructor(size, num) {
-      this.size = size;
-      this.num = num;
-    }
-  
-    // Getter for size
-    getSize() {
-      return this.size;
-    }
-  
-    // Getter for num
-    getNum() {
-      return this.num;
-    }
-  
-    // Method to perform some operation related to pack size
-    displayInfo() {
-      console.log(`Pack Size: ${this.size}, Number: ${this.num}`);
-    }
-  }
+  $("#calculate-btn").click(function () {
+    // Collect data from form input
+    var newPackSize = $("input[name='newpacksize']").val();
+
+    // Create data object
+    var data = {
+      totalNumberOfPacks: Number(newPackSize)
+    };
+
+    // Perform AJAX request
+    $.ajax({
+      type: "POST",
+      url: "/calculatePacks",
+      data: JSON.stringify(data),
+      success: function (response) {
+        // Update the table with the calculated packs
+       // $("tbody").html(""); // Clear existing rows
+        response.forEach(function (pack) {
+          $("#pack-calculate-form > table > tbody").append("<tr><td>" + pack.num + "</td>" + "<td>" + pack.size + "</td></tr>");
+        });
+      },
+      error: function (error) {
+        console.error(error);
+        // Handle error
+      }
+    });
+  });
+
+});
